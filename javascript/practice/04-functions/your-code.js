ae068d312a1a4458a241cc7b4f904440c3a652f9
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-02 09:58:06 -0400

    completed the defining functions practice

--- javascript/practice/04-functions/your-code.js
+++ javascript/practice/04-functions/your-code.js
@@ -8,7 +8,7 @@
 // Write code to add firstNumber and secondNumber and then return the result.
 
 function addTwoNumbers(firstNumber, secondNumber) {
-    // your code goes here
+    return firstNumber + secondNumber;   
 }
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
@@ -31,7 +31,7 @@ console.log("-53 + 40 -> expected: -13, actual: %s", result);
 // Write code to return the first character in text.
 // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Character_access
 function getFirstCharacter(text) {
-    // your code goes here.
+    return text[0];
 }
 
 console.log("getFirstCharacter =====");
@@ -48,12 +48,16 @@ console.log("'Olympus Mons' -> expected: O, actual: %s.", getFirstCharacter("Oly
 // the correct result.
 // Write your code here:
 
-// Uncomment the code below to verify your function is working.
-// console.log("max =====");
-// console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max(-1, 0, 1));
-// console.log("max(11, 2, 1) -> expected: 11, actual: %s", max(11, 2, 1));
-// console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max(-15.67, 33.293, 0.51));
-// console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max(4, 987654321, 0));
+function max(a, b, c) {
+    return Math.max(a, b, c);
+}
+
+//Uncomment the code below to verify your function is working.
+console.log("max =====");
+console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max(-1, 0, 1));
+console.log("max(11, 2, 1) -> expected: 11, actual: %s", max(11, 2, 1));
+console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max(-15.67, 33.293, 0.51));
+console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max(4, 987654321, 0));
 
 
 
