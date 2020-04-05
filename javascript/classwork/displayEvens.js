e73b019be1243bf0b58e339bb181cd2467a52d79
Kristina Zakharova <zakharova.kr@gmail.com>
2020-04-04 21:52:13 -0400

    added another if-statement in case there's no even numbers to display

--- javascript/classwork/displayEvens.js
+++ javascript/classwork/displayEvens.js
@@ -7,14 +7,14 @@ function validate(){
 
     var results = document.getElementById("results");
 
-    // resets validity
-    endNum.setCustomValidity('');							
-	step.setCustomValidity('');
-
     //removes any validation styles that were activated by a previous validation
     displayEvensForm.className = "needs-validation";
     results.style.display = "none"; //hides the results div
     document.getElementById("answers").innerHTML = "";	// resets the answers
+
+    // resets validity
+    endNum.setCustomValidity('');							
+	step.setCustomValidity('');
 
     //we parse each input's value to an integer 
     var numStart = parseInt(startNum.value, 10);
@@ -25,12 +25,12 @@ function validate(){
 
     if (numEnd <= numStart) {						
 		displayEvensForm.className = "was-validated";
-		endNum.setCustomValidity("prob with end num");
+		endNum.setCustomValidity(" ");
 	}
 	
 	if (stepCount<= 0) {
 		displayEvensForm.className = "was-validated";
-		step.setCustomValidity("prob with step num");
+		step.setCustomValidity(" ");
 		return false;
 	}
     if (!displayEvensForm.checkValidity()) {
@@ -48,9 +48,17 @@ function validate(){
         if (i % 2 == 0) {
             document.getElementById("answers").innerHTML += i + "<br />";
         }
+        
         // display evens and user input
 		results.style.display = "block";									
-	}
+    }
+    
+    //added another if-statement in case there's no even numbers to display.
+    //not sure if my logic is correct though
+    if ((numStart % 2 ==1) && (stepCount %2 ==0)) {
+        document.getElementById("resultsP").style.display = "none";
+        document.getElementById("answers").innerText = "Oops, no evens found";
+    }
     
 
     return false;
